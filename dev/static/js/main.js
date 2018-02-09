628bd944533afdbe3693e1fdfff35f3929df1c16
Mikhail <mexcaptain@gmail.com>
2018-02-09 10:59:36 +0300

    Third section

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -1,3 +1,36 @@
 $(document).ready(function () {
-    svg4everybody({});
+    slider();
 });
+
+function slider() {
+    var slider = $('.js-slider'),
+        total = slider.find('> *').length;
+
+    slider.slick({
+        'autoplay': 'false',
+        'dots': 'true'
+    });
+
+    if (total < 10) {
+        total = '0' + total;
+    }
+    $('.js-slider-total').text(total);
+
+
+    slider.on('afterChange', function(event, slick, currentSlide, nextSlide){
+        console.log(currentSlide);
+        if (currentSlide++ < 9) {
+            currentSlide = '0' + currentSlide;
+        }
+        $('.js-slider-current').text(currentSlide);
+    });
+
+    $('.js-slider-prev').on('click', function (e) {
+        e.preventDefault();
+        $('.js-slider').find('.slick-prev').click();
+    });
+    $('.js-slider-next').on('click', function (e) {
+        e.preventDefault();
+        $('.js-slider').find('.slick-next').click();
+    });
+}
